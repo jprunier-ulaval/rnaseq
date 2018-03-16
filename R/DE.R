8f1443efe21f267e0b20a82eae9bd6b12ff88e38
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2018-03-16 11:41:40 -0400

    Added deseq2_analysis function
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

---
+++ R/DE.R
@@ -0,0 +1,26 @@
+#' DESeq2 analysis
+#'
+#' @param txi The txi object returned by the import_kallisto function.
+#' @param design The experimental design (see ?DESeqDataSetFromTximport).
+#' @param formula The design formula in data.frame format (see
+#'                ?DESeqDataSetFromTximport).
+#' @param contrast The contrast for the comparison (see ?DESeq2::results).
+#' @param filter The minimum number of reads detected for a feature across all
+#'               samples. Default: 2
+#'
+#' @return A DESeqDataSet object.
+#'
+#' @examples
+#' txi <- get_demo_txi()
+#' design <- get_demo_design()
+#' dds <- deseq2_analysis(txi, design, ~ group) 
+#' de <- DESeq2::results(dds, contrast = c("group", "A", "B"))
+#'
+#' @import DESeq2
+#'
+#' @export
+deseq2_analysis <- function(txi, design, formula, filter = 2) {
+    dds <- DESeqDataSetFromTximport(txi, design, formula)
+    dds <- dds[rowSums(counts(dds)) >= filter]
+    dds <- DESeq(dds)
+}
--- R/demo.R
+++ R/demo.R
@@ -25,3 +25,16 @@ get_demo_txi <- function() {
     abundances <- get_demo_abundance_files()
     import_kallisto(abundances, anno = "Hs.Ensembl79")
 }
+
+#' Get demo design
+#' 
+#' @return A data.frame corresponding to the demo txi object. See
+#'         ?get_demo_txi.
+#' 
+#' @examples
+#' design <- get_demo_design()
+#'
+#' @export
+get_demo_design <- function() {
+    data.frame(group = c("A", "A", "B", "B"), sample = letters[1:4])
+}
