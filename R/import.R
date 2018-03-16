179fff35c009be648d5b3a369a02fe512f66990c
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2018-03-16 15:53:04 -0400

    Now using data instead of inst/extdata for annotations
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

--- R/import.R
+++ R/import.R
@@ -27,14 +27,8 @@ import_kallisto <- function(filenames, anno = "Hs.Ensembl91", txOut = FALSE,
     valid_anno <- c("Hs.Ensembl91", "Hs.Ensembl79")
     stopifnot(anno %in% valid_anno)
     if (stringr::str_detect(anno, "Ensembl")) {
-        if (anno == "Hs.Ensembl91") {
-            anno <- system.file("extdata/Homo_sapiens.GRCh38.91.all.anno.csv.gz", package="rnaseq")
-        }
-        if (anno == "Hs.Ensembl79") {
-            anno <- system.file("extdata/Homo_sapiens.GRCh38.79.all.anno.csv.gz", package="rnaseq")
-        }
-        anno <- readr::read_csv(anno, col_types = "ciiicccnicccccccccccccccccc")
-        tx2gene <- dplyr::select(anno, TXNAME = transcript_id, GENEID = gene_id)
+        anno <- get(anno)
+        tx2gene <- dplyr::select(anno, TXNAME = id, GENEID = ensembl_gene)
     }
     if (txOut == TRUE) {
         tximport(filenames, type = "kallisto", tx2gene = tx2gene, txOut = TRUE,
