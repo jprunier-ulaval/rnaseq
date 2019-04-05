b0eddd393a405e47c96d14333e5bcba591c063b6
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2019-04-05 13:50:54 -0400

    Added the get_anno_df function
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

--- R/format.R
+++ R/format.R
@@ -1,4 +1,4 @@
-#' Extract raw counts with annotation data.frame from txi 
+#' Extract raw counts with annotation data.frame from txi
 #'
 #' @param txi: The txi object returned from the `import_kallisto` function
 #'
@@ -20,7 +20,7 @@ get_raw_count_anno_df <- function(txi) {
         dplyr::select(one_of(colnames(txi$anno)), everything())
 }
 
-#' Extract TPM with annotation data.frame from txi 
+#' Extract TPM with annotation data.frame from txi
 #'
 #' @param txi: The txi object returned from the `import_kallisto` function
 #'
@@ -37,6 +37,29 @@ get_raw_count_anno_df <- function(txi) {
 #' @export
 get_tpm_anno_df <- function(txi) {
     as.data.frame(txi$abundance) %>%
+        rownames_to_column("id") %>%
+        left_join(txi$anno, by = "id") %>%
+        dplyr::select(one_of(colnames(txi$anno)), everything())
+}
+
+#' Extract FPKM with annotation data.frame from txi
+#'
+#' @param txi: The txi object returned from the `import_kallisto` function
+#' @param colname: The txi object returned from the `import_kallisto` function
+#'
+#' @return A `data.frame` object.
+#'
+#' @examples
+#' abundances <- get_demo_abundance_files()
+#' txi <- import_kallisto(abundances, anno = "Hs.Ensembl79")
+#' fpkm <- get_fpkm_anno_df(txi)
+#'
+#' @import tibble
+#' @import dplyr
+#'
+#' @export
+get_anno_df <- function(txi, col_name) {
+    as.data.frame(txi$fpkm) %>%
         rownames_to_column("id") %>%
         left_join(txi$anno, by = "id") %>%
         dplyr::select(one_of(colnames(txi$anno)), everything())
