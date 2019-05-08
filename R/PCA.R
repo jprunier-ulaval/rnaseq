803ffb671c0863a434992ed6db214c8e88a0bc83
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2019-05-08 16:30:12 -0400

    Added the plot_pca function
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

--- R/PCA.R
+++ R/PCA.R
@@ -1,14 +1,17 @@
-#' Produce a PCA plot
+#' Produce a PCA plot from txi
 #'
-#' @param txi The txi object returned by the import_kallisto function.
+#' @param txi The \code{txi} object returned by the \code{import_kallisto}
+#' function.
+#' @param txi Produce the graph. \code{TRUE} or \code{FALSE}. Default:
+#' \code{TRUE}.
 #'
-#' @return Produce the PCA and silently returns the data.frame used.
+#' @return Produce the PCA and silently returns the \code{data.frame} and the
+#' result from the \code{PCA} function as a \code{list} with 2 elements
+#' (\code{df} and \code{pca}).
 #'
 #' @examples
 #' txi <- get_demo_txi()
-#' \dontrun{
-#' df <- produce_pca(txi)
-#' }
+#' df <- produce_pca(txi, graph = FALSE)
 #'
 #' @import FactoMineR
 #' @import dplyr
@@ -17,7 +20,7 @@
 #' @import ggrepel
 #'
 #' @export
-produce_pca <- function(txi) {
+produce_pca <- function(txi, graph = TRUE) {
     tpm <- as.data.frame(txi$abundance) %>%
         mutate(ensembl_gene = rownames(txi$abundance)) %>%
         tidyr::gather(sample, tpm, -ensembl_gene) %>%
@@ -43,14 +46,33 @@ produce_pca <- function(txi) {
     df <- df %>%
         mutate(sample = rownames(df)) %>%
         tbl_df
+    p <- plot_pca(df)
+    print(p)
 
-    p <- ggplot(df, aes(x = Dim1, y = Dim2)) +
+    invisible(list(df = df, pca = pca))
+}
+
+#' Produce a PCA plot from produce_pca results
+#'
+#' @param txi The txi object returned by the import_kallisto function.
+#'
+#' @return Returns the \code{ggplot} object
+#'
+#' @examples
+#' txi <- get_demo_txi()
+#' res_pca <- produce_pca(txi, graph = FALSE)
+#' p <- plot_pca(res_pca$df, res_pca$pca)
+#'
+#' @import tidyr
+#' @import ggplot2
+#' @import ggrepel
+#'
+#' @export
+plot_pca <- function(df, pca) {
+    ggplot(df, aes(x = Dim1, y = Dim2)) +
         geom_point(size = 3) +
         geom_text_repel(aes(label = sample), color = "black", force = 10) +
         theme_bw() +
         xlab(paste0("Dim1 (", pca$eig[1,2] %>% round(2), "%)")) +
         ylab(paste0("Dim2 (", pca$eig[2,2] %>% round(2), "%)"))
-    print(p)
-    invisible(df)
 }
-
